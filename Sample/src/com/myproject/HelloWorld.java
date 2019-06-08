f9b2a6a2c9217e0cbfece090e96159258cac62f4
pradhan <pradhan@gmail.com>
2019-06-08 15:16:42 -0400

    adding variables string and num

--- Sample/src/com/myproject/HelloWorld.java
+++ Sample/src/com/myproject/HelloWorld.java
@@ -1,7 +1,11 @@
 package com.myproject;
 
 public class HelloWorld {
-
+	
+	private String text1;
+	private String text2;
+	private int num;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
